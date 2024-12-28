2b2e56ad93d327063dbc7873517190a5075574d4
Oluwa-ios <50711478+oluwatobiHammed@users.noreply.github.com>
2024-12-27 20:41:16 -0500

    added Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.5
+
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "_Pods",
+    products: [
+        .library(
+            name: "_Pods",
+            targets: ["_Pods"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "_Pods",
+            dependencies: [])
+    ]
+)
